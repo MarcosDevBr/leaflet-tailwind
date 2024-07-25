577d713888b281cb5a2cfd9c919f0d337bec8aec
Marcos dev <marcosagusto1041@gmail.com>
2024-07-25 16:57:05 -0300

    criação projeto com Vite, Tailwind CSS, TypeScript

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+   <main className="py-10 bg-blue-50 h-screen">
+    <h1 className="font-bold text-3xl text-center">
+      Wello Hord!
+    </h1>
+   </main>
+  )
+}
+
+export default App
