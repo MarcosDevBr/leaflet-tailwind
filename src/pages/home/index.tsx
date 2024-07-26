7a6327ee78d95845287ad479f829c0bc221b1175
Marcos dev <marcosagusto1041@gmail.com>
2024-07-25 23:34:38 -0300

    feature: criação da pagina home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "./style";
+import { IMapProps } from "./model";
+
+export default function Home(_props: IMapProps) {
+    return (
+        <Container>
+            <h1>
+                Home Screem
+            </h1>
+        </Container>
+    )
+}
